bea8696ebd3da8d0ea4ee38ef3993a4cae851eea
Ritwik Das <dasritrwik16@gmail.com>
2022-05-06 01:44:06 +0530

    Solved 2

--- 2_MaxMinArray.cpp
+++ 2_MaxMinArray.cpp
@@ -1,7 +1,7 @@
 /*
 * Problem: Find the maximum and minimum element in an array
 * Topic: Array
-* Function: void findMinMax(int*, int);
+* Function: pair<int, int> findMinMax(int*, int);
 * Sample Input: [5,6,1,3]
 * Sample Output: 1,6
 * Complexity: O(n/2) == O(n)
@@ -16,15 +16,44 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void findMinMax(int*, int);
+pair<int, int> findMinMax(int*, int);
 
-void main(){
+int main(){
 
+    int arr[4] = {5,6,1,3}; //IP
+    int size = sizeof(arr)/sizeof(arr[0]); //Calculating Size
+
+    pair<int, int> p;
+    p = findMinMax(arr, size);
+
+    cout << p.first << " " << p.second;
+    return 0;
 }
 
 
 
-void findMinMax(int *arr, int size){
+pair<int, int> findMinMax(int *arr, int size){
+    int min=0, max =0;
+
+    if(arr[0]>arr[1]){
+        min = arr[1];
+        max = arr[0];
+    }else{
+        min = arr[0];
+        max = arr[1];
+    }
+
+    for(int i=2; i<size-1; i++){
+
+        if(arr[i]>max){
+            max = arr[i];
+        }
+        else if(arr[i] < min){
+            min = arr[i];
+        }
+    }
+
+    return make_pair(min,max);
 
     
 }
